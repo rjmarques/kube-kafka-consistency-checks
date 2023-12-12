43f0196f57b7d85ce0308f18416afd9ff4f455f7
rjmarques <ricardomarques377@gmail.com>
2023-12-12 17:37:12 +0000

    consistent handling of messages

--- src/controller/__main__.py
+++ src/controller/__main__.py
@@ -1,36 +1,111 @@
-
-from confluent_kafka import Producer
+from confluent_kafka import Consumer, Producer, KafkaError, KafkaException
+import time
 import threading
 import socket
-from time import sleep
-from utils.time import millis_to_datetime
+import uuid
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.INFO)
+
+# indicates if message production is enabled
+stopped = False
+
+# map of inflight messages: { uuid -> epoch seconds timestamp }
+inflight_messages = {}
+
+def gen_value() -> str:
+    return str(uuid.uuid4())
 
 def acked(err, msg):
     if err is not None:
-        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+        logging.info("failed to deliver message: %s: %s" % (str(msg), str(err)))
     else:
-        print(f"Message produced: {msg.topic()} @ {msg.partition()} ({msg.offset()}) - {millis_to_datetime(msg.timestamp()[1])}")
+        logging.debug(f"message produced: {msg.topic()} @ {msg.partition()} ({msg.offset()}) -> {msg.value().decode()}")
 
-def produce_loop(prd, topic):
+def produce_loop(producer, topic):
+    if stopped:
+        return # stop the production
+
     # push one record to each partition
     for i in range(4):
-        prd.produce(topic, partition=i, value=f"foo_{i}", on_delivery=acked)
-        sleep(1)
+        val = gen_value()
+        producer.produce(topic, partition=i, value=val, on_delivery=acked)
+        inflight_messages[val] = time.time()
+        time.sleep(1)
     
-    prd.flush()
-    threading.Timer(1, produce_loop(prd, topic)).start()
+    producer.flush()
+    threading.Timer(1, produce_loop, (producer, topic)).start()
+
+def consume_loop(consumer, topic):
+    global stopped
+
+    try:
+        consumer.subscribe(topics=[topic])
+
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None: continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    logging.info.write('%% %s [%d] reached end at offset %d\n' % (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                id = msg.value().decode()
+                logging.debug(f"message received: {msg.topic()} @ {msg.partition()} ({msg.offset()}) -> {id}")
+                if id in inflight_messages:
+                    del inflight_messages[id]    
+                else:
+                    logging.info(f"{id} not found in map!")
+                    
+    except RuntimeError as err:
+        logging.info("err raised in consume loop:", err)
+    
+    finally:
+        # close down consumer to commit final offsets.
+        logging.info("terminating consume loop")
+        stopped = True
+        consumer.close()
+
+def check_for_stuck():
+    logging.info("checking for late/stuck messages")
+    
+    now = time.time()
+    for id, ts in inflight_messages.items():
+        age_in_seconds = now - ts
+        if age_in_seconds > 30:
+            logging.info(f'{id} is stil inflight after {age_in_seconds} seconds!')
+    
+    threading.Timer(10, check_for_stuck).start()
+
 
 def main():
-    conf = {
+    producer = Producer({
         'bootstrap.servers': 'localhost:29092',
         'client.id': socket.gethostname()
-    }
+    })
+    input_topic = 'consistency-checks-in'
 
-    producer = Producer(conf)
-    topic = 'consistency-checks-in'
-    
+    consumer = Consumer({
+        'bootstrap.servers': 'localhost:29092',
+        'enable.auto.commit': True,
+        'group.id': 'consistency-controller',
+        'auto.offset.reset': 'earliest',
+        'isolation.level': 'read_committed'
+    })
+    output_topic = 'consistency-checks-out'
+
     # start the production
-    produce_loop(producer, topic)
+    threading.Timer(1, produce_loop, (producer, input_topic)).start()
+
+    # periodically check for stuck messages
+    threading.Timer(10, check_for_stuck).start()
+
+    # start the consumption
+    consume_loop(consumer, output_topic)
+
 
 if __name__ == "__main__":
     main()
--- src/worker/__main__.py
+++ src/worker/__main__.py
@@ -1,46 +1,137 @@
-from confluent_kafka import Consumer, KafkaError, KafkaException
-from utils.time import millis_to_datetime
+from confluent_kafka import Consumer, Producer, KafkaError, KafkaException
+import uuid
+import logging
+import time
+import random
+import threading
 
-def print_assignment(consumer, partitions):
+logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.INFO)
+
+def delivery_report(err, msg):
+    if err:
+        logging.info(f'message delivery failed ({msg.topic()} [{str(msg.partition())}]): {err}')
+    else:
+        logging.info(f"message delivered: {msg.topic()} @ {msg.partition()} ({msg.offset()}) -> {msg.value().decode()}")
+
+uncommited_count = 0
+
+def on_assign(consumer, partitions):        
     for p in partitions:
-        print(f'Assigned: topic {p.topic} partition {p.partition} offset {p.offset}')
+        logging.info(f'assigned: topic {p.topic} partition {p.partition} offset {p.offset}')
 
-def consume_loop(consumer, topic):
+
+def handle_revokation(producer):
+    def on_revoke(consumer, partitions):
+        for p in partitions:
+            logging.info(f'revoking: topic {p.topic} partition {p.partition} offset {p.offset}')
+
+        if uncommited_count > 0:
+            try:
+                commit_transaction(producer, consumer)         
+            except:
+                abort_transaction(producer)                
+
+    return on_revoke
+
+def commit_transaction(producer, consumer):
+    # serve delivery reports from previous produce()
+    producer.poll(0)
+
+    logging.info('committing transaction....')
+
+    # add the commit of the consumed offsets to the transaction
+    producer.send_offsets_to_transaction(
+        consumer.position(consumer.assignment()),
+        consumer.consumer_group_metadata()
+    )
+
+    # commit the transaction
+    producer.commit_transaction()
+
+    # new transaction for further commits
+    begin_transaction(producer)
+
+def abort_transaction(producer):
+    producer.abort_transaction()
+    
+    begin_transaction(producer)
+
+def begin_transaction(producer):
+    global uncommited_count
+
+    # begin new transaction
+    producer.begin_transaction()
+
+    # reset the counter
+    uncommited_count = 0
+
+def transform(in_msg) -> str:
+    return in_msg.value().decode()
+
+# returns true X% of the time
+def should_raise() -> bool:
+    return random.randint(1, 100) <= 1
+
+def transform_loop(consumer, input_topic, producer, output_topic):
+    global uncommited_count
+
     try:
-        consumer.subscribe(topics=[topic], on_assign=print_assignment)
+        # init the transaction, clears out any other ones for the given transaction.id
+        producer.init_transactions()
 
+        consumer.subscribe(topics=[input_topic], on_assign=on_assign, on_revoke=handle_revokation(producer))
+                
+        begin_transaction(producer)
+
+        logging.info("starting Consume-Transform-Process loop")
         while True:
-            msg = consumer.poll(timeout=1.0)
+            msg = consumer.poll(timeout=0.100)
             if msg is None: continue
 
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
                     # End of partition event
-                    print.write('%% %s [%d] reached end at offset %d\n' % (msg.topic(), msg.partition(), msg.offset()))
+                    logging.info.write('%% %s [%d] reached end at offset %d\n' % (msg.topic(), msg.partition(), msg.offset()))
                 elif msg.error():
                     raise KafkaException(msg.error())
             else:
-                print(f"Message received: {msg.topic()} @ {msg.partition()} ({msg.offset()}) - {millis_to_datetime(msg.timestamp()[1])}")
-                consumer.commit(asynchronous=False)
-                # TODO msg process
+                logging.info(f"message received: {msg.topic()} @ {msg.partition()} ({msg.offset()}) -> {msg.value().decode()}")
+                out_msg = transform(msg)
+                producer.produce(output_topic, value=out_msg, on_delivery=delivery_report)
+
+                # keep track of how many messages need to be commited in the transaction
+                uncommited_count += 1
+
+            # synthetic random error to force some transactions to fail sporadically
+            if should_raise():
+                raise RuntimeError("random error! Boom!")
+            
+            if uncommited_count >= 10:
+                commit_transaction(producer, consumer)
 
     finally:
-        # Close down consumer to commit final offsets.
+        # close down consumer to commit final offsets.
         consumer.close()
 
 def main():
-    conf = {
+    consumer = Consumer({
         'bootstrap.servers': 'localhost:29092',
-        'enable.auto.commit': 'false',
+        'enable.auto.commit': False,
         'group.id': 'consistency-worker',
-        'auto.offset.reset': 'earliest'
-    }
+        'auto.offset.reset': 'earliest',
+        'isolation.level': 'read_committed'
+    })
 
-    consumer = Consumer(conf)
-    topic = 'consistency-checks-in'
+    producer = Producer({
+        'bootstrap.servers': 'localhost:29092',
+        'transactional.id': f'eos-consistency-{str(uuid.uuid4())}' # exactly-once-semantics
+    })
+
+    input_topic = 'consistency-checks-in'
+    output_topic = 'consistency-checks-out'
     
     # start the production
-    consume_loop(consumer, topic)
+    transform_loop(consumer, input_topic, producer, output_topic)
 
 if __name__ == "__main__":
     main()
